6352b2f9b172ff51d0ed8350cab71cfc0c04844c
mamadousoko <mamadousoko@gmail.com>
2024-05-14 05:02:48 +0000

    hello

---
+++ src/test/java/com/cicdpipe/devopstest/DevopstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdpipe.devopstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
